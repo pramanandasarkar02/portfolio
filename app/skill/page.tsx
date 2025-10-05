bb175ee06dbcf78540598533efdd662ab6e7aca2
pramanandasarkar02 <pramanandasarkar02@gmail.com>
2025-10-05 21:34:56 +0600

    languages skill updated

---
+++ app/skill/page.tsx
@@ -0,0 +1,236 @@
+import { FaC, FaCartPlus, FaGolang, FaJava, FaPython, FaRust } from "react-icons/fa6"
+import { SiCplusplus, SiHaskell, SiJavascript, SiTypescript } from "react-icons/si"
+
+
+const page = () => {
+  return (
+    <div>
+        Skills
+        <div>
+            <p>Programming Languages:</p>
+            <div>
+                <FaRust />
+                <h1>Rust</h1>
+                <div>
+                    <h2>details</h2>
+                    <ol>
+                        <li>build more secure application with peak performance.</li>
+                        <li>Languages of feeling and think it is greatest language every created.</li>
+                        <li>Build a royal application with heart</li>
+                    </ol>
+                </div>
+                <div>
+                    <h2>Frameworks and library</h2>
+                    <ol>
+                        <li>
+                            <h2>REST</h2>
+                            <ul>
+                                <li>
+                                    Axum
+                                </li>
+                                <li>
+                                    SQLx
+                                </li>
+                                <li>
+                                    Tokio
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>
+                                Realtime/websocket
+                            </h2>
+                            <ul>
+                                <li>Actix</li>
+                                <li>Redis</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>Fulll stack</h2>
+                            <ul>
+                                <li>Leptos</li>
+                                <li>Axum</li>
+                                <li>Postgress</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>AI/ML</h2>
+                            <ul>
+                                <li>Candle</li>
+                                <li>Burn</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>MIcroservice</h2>
+                             <ul>
+                                <li>WasmCloud</li>
+                                <li>NATS.rs</li>
+                             </ul>
+                        </li>
+                        <li>
+                            <h2>System apps</h2>
+                            <ul>
+                                <li>Tauri</li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <div>
+                <FaJava />
+                <h1>Java</h1>
+                <div>
+                    <h2>details</h2>
+                    <ol>
+                        <li>Use when need more professional.</li>
+                        <li>As a first learned languages and having industry demand I can do what it need</li>
+                        <li>Build scalable service in java. I perferd to use it</li>
+                    </ol>
+                </div>
+                <div>
+                    <h2>Frameworks and library</h2>
+                    <ol>
+                        <li>
+                            <h2>REST</h2>
+                            <ul>
+                                <li>
+                                    Spring Boot
+                                </li>
+                                <li>
+                                    Spring Data JPA
+                                </li>
+                                <li>
+                                    Hybernet
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>
+                                Realtime/websocket
+                            </h2>
+                            <ul>
+                                <li>Webflux</li>
+
+                            </ul>
+                        </li>
+                        
+                        
+                        <li>
+                            <h2>MIcroservice</h2>
+                             <ul>
+                                <li>Spring Claude</li>
+                                <li>Eureka</li>
+                                <li>Kafka</li>
+                             </ul>
+                        </li>
+                        <li>
+                            <h2>System apps</h2>
+                            <ul>
+                                <li>JavaFX</li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <div>
+                <FaGolang />
+                <h1>Golang</h1>
+                <div>
+                    <h2>details</h2>
+                    <ol>
+                        <li>build more secure application with peak performance.</li>
+                        <li>Languages of feeling and think it is greatest language every created.</li>
+                        <li>Build a royal application with heart</li>
+                    </ol>
+                </div>
+                <div>
+                    <h2>Frameworks and library</h2>
+                    <ol>
+                        <li>
+                            <h2>REST</h2>
+                            <ul>
+                                <li>
+                                    Axum
+                                </li>
+                                <li>
+                                    SQLx
+                                </li>
+                                <li>
+                                    Tokio
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>
+                                Realtime/websocket
+                            </h2>
+                            <ul>
+                                <li>Actix</li>
+                                <li>Redis</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>Fulll stack</h2>
+                            <ul>
+                                <li>Leptos</li>
+                                <li>Axum</li>
+                                <li>Postgress</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>AI/ML</h2>
+                            <ul>
+                                <li>Candle</li>
+                                <li>Burn</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>MIcroservice</h2>
+                             <ul>
+                                <li>WasmCloud</li>
+                                <li>NATS.rs</li>
+                             </ul>
+                        </li>
+                        <li>
+                            <h2>System apps</h2>
+                            <ul>
+                                <li>Tauri</li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <div>
+                <SiHaskell />
+                <h1>Haskell</h1>
+            </div>
+
+            <div>
+                <FaPython />
+                <h1>Python</h1>
+            </div>
+            
+            <div>
+                <SiJavascript />
+                <h1>Javascript</h1>
+                <SiTypescript />
+                <h1>TypeScript</h1>
+            </div>
+            <div>
+                <SiTypescript />
+                <h1>TypeScript</h1>
+            </div>
+            <div>
+                <FaC />
+                <h1>C</h1>
+            </div>
+            <div>
+                <SiCplusplus />
+                <h1>C++</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
